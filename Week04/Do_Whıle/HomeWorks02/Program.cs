6c2741fd9cdb923bccedf65f4175021aeffe25eb
haticebasal1 <152809295+haticebasal1@users.noreply.github.com>
2025-02-21 17:04:43 +0300

    odev

---
+++ Week04/Do_Whıle/HomeWorks02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Bir cümle giriniz:");
+string str = Convert.ToString(Console.ReadLine()!);
+    while (str!="0")
+    {
+        string buyuk=str.ToUpper();
+        Console.WriteLine("Büyük harfle:"+buyuk);
+        break;
+    }
+
+
